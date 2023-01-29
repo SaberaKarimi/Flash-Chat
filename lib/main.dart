6b40c49a7d2bc15138b55e8c93c7133892aec95b
SaberaKarimi <saberakarimi85@gmail.com>
2023-01-28 21:59:16 -0800

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flash_chat_starting_project/firebase_options.dart';
 import 'package:flash_chat_starting_project/screens/chat_screen.dart';
 import 'package:flash_chat_starting_project/screens/login_screen.dart';
 import 'package:flash_chat_starting_project/screens/registration_screen.dart';
@@ -7,7 +9,11 @@ import 'constants.dart';
 import 'package:flutter/material.dart';
 import '/screens/welcome_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(FlashChat());
 }
 
